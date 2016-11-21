89c5597b18048082dbf8c2c704a7ad739865a816
Kevin Mahoney <kjmahoney13@gmail.com>
2016-11-21 14:25:00 -0500

    rendered angular index

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,20 @@
+console.log("app.js connected")
+
 angular
   .module("cards", [
     "ui.router",
     "ngResource",
   ])
+  .config([
+    "$stateProvider",
+    Router
+  ])
+
+  function Router($stateProvider){
+    console.log("router working")
+    $stateProvider
+    .state("index",{
+      url: "/",
+      templateUrl :"./js/ng-views/index.html",
+    })
+  }
