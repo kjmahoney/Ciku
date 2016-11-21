aafc3da675032caf9188f09958db8a357efd1209
Kevin Mahoney <kjmahoney13@gmail.com>
2016-11-21 12:05:59 -0500

    rendered index page

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
-var mongoose = require("db/connection.js")
-var express = require("express")
-mongoose.models = require("db/models.js")
+var mongoose      = require("./db/connection")
+var models        = require("./db/models")
+var express       = require("express")
+var parser        = require("body-parser")
+var hbs           = require ("hbs")
+var Deck          = mongoose.model("Deck")
+var Card          = mongoose.model("Card")
+var app           = express()
+
+//Sets
+app.set("port", process.env.PORT || 4002)
+app.set("view engine", "hbs")
+
+//Route to home/index to display all decks
+app.get("/", (req,res)=>{
+  console.log("I used to be like you")
+  Deck.find({}).then(decks =>{
+    res.render("index",{
+      decks: decks
+    })
+  })
+})
+
+
+app.listen(app.get("port"), function(){
+  console.log("consequences")
+})
