7a23c9d55d71401285c98bbad8c253c09a7bb931
Kevin Mahoney <kjmahoney13@gmail.com>
2017-01-10 15:50:04 -0500

    fix NaN percentage bug

--- public/js/logic/percentLearned.js
+++ public/js/logic/percentLearned.js
@@ -8,7 +8,11 @@ percentLearned= function(deck){
     }
   }
   //calculate percentage of cards learned to the cards in the deck
+  if (deck.cards.length == 0){
+  return percentage = 0
+}else{
   return percentage = Math.ceil(((learned.length/deck.cards.length)*100))
+}
 }
 
 //provide number of cards in the deck
